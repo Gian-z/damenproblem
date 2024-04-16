7ef1d9755c19e9167f4a5b80714388ea0fb9b1c5
GioeleAmbrogini <gioele.ambrogini@edu.tbz.ch>
2024-04-16 09:04:37 +0200

    added input

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,6 +2,14 @@ package m323.damenproblem
 
 object Main {
   def main(args: Array[String]): Unit = {
-    println("Hello world!")
+    val int = readInt()
+  }
+
+  def readInt(): Int = {
+    println("How long is your board?")
+    val length = scala.io.StdIn.readInt()
+    println("Your board is " + length + " long")
+
+    length
   }
 }

69b3aacc15a35b2ae54f4a132b02337ec6c54660
Gian Zwahlen <gianluca.zwahlen@cmiag.ch>
2024-04-16 10:45:52 +0200

    add visualizer

---
+++ src/main/scala/Visualizer.scala
@@ -0,0 +1,35 @@
+package m323.damenproblem
+
+import scala.annotation.tailrec
+
+case class Visualizer() {
+
+  def visualize(board: List[Int]): String = {
+    val length = board.length
+    if (length <= 0)
+      return ""
+
+    val separator = getSeparator(length)
+    board.map(i => getLine(length, i)).map(s => s.reverse).foldLeft(separator)(_ + "\n" + _ + "\n" + separator)
+  }
+  
+  @tailrec
+  private def getLine(length: Int, index: Int, previous: String = "|"): String = {
+    if (length <= 0)
+      return previous
+
+    var snippet = "   |"
+    if (length == index + 1)
+      snippet = " ♕ |"
+    
+    getLine(length - 1, index, previous + snippet)
+  }
+
+  @tailrec
+  private def getSeparator(length: Int, previous: String = "+"): String = {
+    if (length <= 0)
+      return previous
+
+    getSeparator(length - 1, previous + "---+")
+  }
+}
